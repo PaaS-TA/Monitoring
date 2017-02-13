588f9ed757796b38c7b2d08ff654b5b348163bc9
ihocho <ihocho@crossent.com>
2017-02-13 22:21:15 +0900

    paasta 2.0 controller & container resource upload

---
+++ releases/paasta-controller-2.0/spec/manifest_generation_spec.rb
@@ -0,0 +1,35 @@
+require 'support/yaml_eq'
+require 'yaml'
+require 'tempfile'
+require 'erb'
+
+describe "Manifest Generation" do
+  shared_examples "generating manifests" do |infrastructure|
+    it "builds the correct manifest for #{infrastructure}" do
+      example_manifest = Tempfile.new("example-manifest.yml")
+      `./scripts/generate_deployment_manifest #{infrastructure} spec/fixtures/#{infrastructure}/cf-stub.yml > #{example_manifest.path}`
+      expect($?.exitstatus).to eq(0)
+
+      expected = File.read("spec/fixtures/#{infrastructure}/cf-manifest.yml")
+      actual = File.read(example_manifest.path)
+
+      expect(actual).to yaml_eq(expected)
+    end
+  end
+
+  context "aws" do
+    it_behaves_like "generating manifests", "aws"
+  end
+
+  context "bosh-lite" do
+    it_behaves_like "generating manifests", "bosh-lite"
+  end
+
+  context "openstack" do
+    it_behaves_like "generating manifests", "openstack"
+  end
+
+  context "vsphere" do
+    it_behaves_like "generating manifests", "vsphere"
+  end
+end
